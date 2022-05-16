05750bd3774626d13782382fb63c04d6a781c109
Nishidha <npanpa23@in.ibm.com>
2022-05-16 20:09:05 +0530

    Gcc11 support (#19)
    * Updated openjdk version to 11
    
    * Updated CDT packages on x86
    
    * Delete nohup.out
    
    * Removed zip CDT recipe
    
    * Minor change

---
+++ java-11-openjdk-headless-cos7-x86_64/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# *****************************************************************
+# (C) Copyright IBM Corp. 2020, 2022. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# *****************************************************************
+set -ex
+source "${SRC_DIR}/build_cdt_package.sh"
+
+SYSROOT_DIR="${PREFIX}"/x86_64-conda_cos7-linux-gnu/sysroot/usr
+
+pre_build
+
+# START OF INSERTED BUILD APPENDS
+
+jvm_slug=$(compgen -G "${SYSROOT_DIR}/lib/jvm/java-11-openjdk-*")
+jvm_slug=$(basename ${jvm_slug})
+
+# Fix broken links
+pushd ${SYSROOT_DIR}/lib/jvm/${jvm_slug}/lib > /dev/null 2>&1
+rm -rf tzdb.dat
+ln -s ${SYSROOT_DIR}/share/javazi-1.8/tzdb.dat ${SYSROOT_DIR}/lib/jvm/${jvm_slug}/lib
+popd > /dev/null 2>&1
+
+
+# END OF INSERTED BUILD APPENDS
+post_build
+
